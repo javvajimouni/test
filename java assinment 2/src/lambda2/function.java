70f383d3ed2fdc9ab787e38e907a5d273472771f
mjavvaji <mjavvaji@LIN66008339.corp.capgemini.com>
2022-07-11 12:57:51 +0530

    lambda assignments

---
+++ java assinment 2/src/lambda2/function.java
@@ -0,0 +1,10 @@
+package lambda2;
+import java.util.function.Function;
+
+public class function {
+	public static void main(String[] args) {
+	       Function<String,Integer> function=(t)->t.length();
+	       System.out.println(function.apply("Hello word"));
+		}
+
+}
---
+++ java assinment 2/src/lambda2/supplier.java
@@ -0,0 +1,11 @@
+package lambda2;
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+public class supplier {
+	public static void main(String[] args) {
+	     Supplier<LocalDateTime> supplier =()->LocalDateTime.now();
+	     System.out.println(supplier.get());
+		}
+
+
+}
