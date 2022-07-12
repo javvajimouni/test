891c13210509068c7a4b579bf6f15ff07296fdc5
mjavvaji <mjavvaji@LIN66008339.corp.capgemini.com>
2022-07-12 11:23:23 +0530

    Java11 done

---
+++ java assinment 2/src/java11/Java11Four.java
@@ -0,0 +1,16 @@
+package java11;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+public class Java11Four {
+	public static void main(String[] args) throws IOException {
+		var path="C:\\Users\\MJAVVAJI\\mouni\\test";
+		String regex = "\\s+";
+	     String data=Files.readString(Path.of(path));
+	     String[] strSplit = data.split(regex);
+	     
+	     System.out.println("Word count is " + strSplit.length);
+	     System.out.println(data);
+	     
+		}
+}
